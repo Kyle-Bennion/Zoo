fe2b9cb974f1e327614f7cb1db6f1327d08af49f
Kyle Bennion <bilekennion@hotmail.com>
2020-08-24 15:12:59 -0600

    Added Animals

---
+++ app/models/Mastiff.js
@@ -0,0 +1,27 @@
+class Mastiff {
+  constructor(
+    name = '',
+    color = '',
+    gender = '',
+    weight = 0,
+    diet = '',
+    sound = '',
+    temperment = '',
+    type = ''
+  ) {
+    this.name = name;
+    this.color = color;
+    this.gender = gender;
+    this.weight = weight;
+    this.diet = diet;
+    this.sound = sound;
+    this.temperment = temperment;
+    this.type = type;
+  }
+  eat(diet) {
+    console.log(`Crunch Crunch all the ${diet}ies.`)
+  }
+  speak(sound) {
+    alert(`the ${this.constructor.name} says${sound}!`)
+  }
+}
---
+++ app/models/XrayTetra.js
@@ -0,0 +1,27 @@
+class XrayTetra {
+  constructor(
+    name = '',
+    color = '',
+    gender = '',
+    weight = 0,
+    diet = '',
+    sound = '',
+    temperment = '',
+    type = ''
+  ) {
+    this.name = name;
+    this.color = color;
+    this.gender = gender;
+    this.weight = weight;
+    this.diet = diet;
+    this.sound = sound;
+    this.temperment = temperment;
+    this.type = type;
+  }
+  eat(diet) {
+    console.log(`blub blub all the ${diet}ies.`)
+  }
+  speak(sound) {
+    alert(`the ${this.constructor.name} says${sound}!`)
+  }
+}
